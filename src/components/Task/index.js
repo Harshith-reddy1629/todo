d314d2eb4035c0f3ae505fc797acc59f834596ad
Harshith-reddy1629 <reddybharshith3@gmail.com>
2023-11-04 07:26:52 +0000

    @1.0.5

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,18 +1,52 @@
 import { useState, useEffect } from "react";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { CiCircleInfo } from "react-icons/ci";
 import "./index.css";
+
+const ExPlaceholders = [
+  "ex:deadline XX-XX-XX ",
+  "ex: important",
+  "ex: -------",
+];
+
 const TaskItem = (props) => {
   const { item } = props;
-  const { task, _id, selected } = item;
+  const { task, _id, selected, createdAt } = item;
+  const oneOfPH = ExPlaceholders[Math.floor(Math.random() * 3)];
 
   return (
     <li className="item-container">
-      <input
-        className="check-input"
-        id={_id}
-        type="checkbox"
-        // checked={selected}
-      />
-      <label htmlFor={_id}>{task}</label>
+      <div className="task-name-container">
+        <input
+          className="check-input"
+          id={_id}
+          type="checkbox"
+          // checked={selected}
+        />
+        <label className="task-label" htmlFor={_id}>
+          {task}
+        </label>
+      </div>
+      <select className="select-input">
+        <option className="task-option">todo</option>
+        <option className="task-option">inprogress</option>
+        <option className="task-option">completed</option>
+      </select>
+      <input className="note-input" placeholder={oneOfPH} />
+      <div className="ud-op-container">
+        <button type="button" className="ud-op-btn">
+          <AiFillEdit size={20} />
+        </button>
+        <button type="button" className="ud-op-btn">
+          <AiFillDelete size={20} />
+        </button>
+      </div>
+      <div className="task-info">
+        <div className="info-container">
+          <p className="info">Created: {createdAt.slice(0, 10)}</p>
+        </div>
+        <CiCircleInfo size={16} />
+      </div>
     </li>
   );
 };
